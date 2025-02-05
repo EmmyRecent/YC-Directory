1d97a6333e0b6f16f779a719cbca09837420b63c
EmmyRecent <chukwuemekaeziagulu@gmail.com>
2025-02-05 15:10:37 +0000

    Add user authentication

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+const layout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
+  return (
+    <main className="font-work-sans">
+      {/* Render a navbar right here */}
+      <Navbar />
+
+      {children}
+    </main>
+  );
+};
+
+export default layout;
---
+++ app/(root)/page.tsx
@@ -0,0 +1,9 @@
+const page = () => {
+  return (
+    <>
+      <h1 className="text-2xl">Home page</h1>
+    </>
+  );
+};
+
+export default page;
